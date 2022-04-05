827a49eb8cddf9aa57f7ac4d096f7d422dd8da43
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-04-05 18:24:56 +0300

    Add changes to follow the requirements

--- person.rb
+++ person.rb
@@ -1,9 +1,9 @@
 class Person
   attr_reader :id
-  attr_accessor :name, :age, :parent_permission
+  attr_accessor :name, :age
 
   def initialize(age, name: 'Unknown', parent_permission: true)
-    @id = id
+    @id = Random.rand(1..100)
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -19,3 +19,10 @@ class Person
     @age >= 18
   end
 end
+
+person = Person.new(31, name: 'John', parent_permission: false)
+
+puts person.name
+puts person.age
+puts person.id
+puts person.can_use_services?
