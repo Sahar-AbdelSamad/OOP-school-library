32f411c72b796539896539399e79d143aa2c3b40
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-04-05 23:46:17 +0300

    Create base Decorator, CapitalizeDecorator and TrimmerDecorator

---
+++ decorator.rb
@@ -0,0 +1,24 @@
+require './nameable'
+
+class Decorator < Nameable
+  def initialize(nameable)
+    super()
+    @nameable = nameable
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    @nameable.correct_name[0, 9] if @nameable.correct_name.length > 10
+  end
+end
