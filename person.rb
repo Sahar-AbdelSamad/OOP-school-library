71477cae533d5856808cbd9f20598a6ca5649968
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-04-08 21:53:41 +0300

    Remove old examples

--- person.rb
+++ person.rb
@@ -33,19 +33,3 @@ class Person < Nameable
     @age >= 18
   end
 end
-
-person = Person.new(22, name: 'maximilianus')
-
-puts person.id
-puts person.age
-puts person.can_use_services?
-puts person.name
-puts person.correct_name
-
-capitalized_person = CapitalizeDecorator.new(person)
-
-puts capitalized_person.correct_name
-
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-
-puts capitalized_trimmed_person.correct_name
