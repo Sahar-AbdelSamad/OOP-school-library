dbe4a1072e3961b09879d64729b222c678e2e9da
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-04-11 23:12:12 +0300

    Follow Single-responsiblity Principle

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ require './teacher'
 require './rental'
 require './person'
 require './checks'
+require_relative 'create_book'
+require_relative 'create_person'
 
 class App
   include Checks
@@ -12,8 +14,8 @@ class App
   def initialize
     @people = []
     @books = []
-    @book = nil
-    @person = nil
+    @create_book = Create_book.new(@books)
+    @create_person = Create_person.new(@people)
     @rents = []
   end
 
@@ -24,9 +26,9 @@ class App
     when 2
       list_people
     when 3
-      create_person
+      @create_person.create_person
     when 4
-      create_book
+      @create_book.create_book
     when 5
       create_rental
     when 6
@@ -44,57 +46,6 @@ class App
     @people.each_with_index do |person, index|
       puts "#{index}) [#{person.class.name}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
-  end
-
-  def create_person
-    loop do
-      print "Do you want to create a student (1) or a teacher (2)? [Input the number]:\s"
-      person = gets.chomp
-      case person
-      when '1'
-        create_student
-      when '2'
-        create_teacher
-      end
-
-      break if %w[1 2].include?(person)
-    end
-
-    puts "Person created successfully\n\n"
-  end
-
-  def create_student
-    age = numeric(message: "Age:\s")
-    name = not_empty(message: "Name:\s")
-    loop do
-      print "Has parent permission [Y/N]?\s"
-      permission = gets.chomp
-      if %w[y Y].include?(permission)
-        student = Student.new(nil, age, name: name, parent_permission: true)
-        @people << student unless @people.include?(student)
-        break
-      elsif %w[n N].include?(permission)
-        student = Student.new(nil, age, name: name, parent_permission: false)
-        @people << student unless @people.include?(student)
-        break
-      end
-    end
-  end
-
-  def create_teacher
-    age = numeric(message: "Age:\s")
-    name = not_empty(message: "Name:\s")
-    specialization = not_empty(message: "Specialization:\s")
-    teacher = Teacher.new(specialization, age, name: name)
-    @people << teacher unless @people.include?(teacher)
-  end
-
-  def create_book
-    title = not_empty(message: "Title:\s")
-    author = not_empty(message: "Author:\s")
-    puts "Book created successfully \n\n"
-    book = Book.new(title, author)
-    @books << book unless @books.include?(book)
   end
 
   def create_rental
